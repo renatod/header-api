0cc4a5fe10fba5b7496b7eb9f75a82ed3b750393
Renato Dias <renato.dias@me.com.br>
2020-10-03 16:33:56 -0300

    ci

--- src/app.js
+++ src/app.js
@@ -146,7 +146,7 @@ app.get('/', (req, res) => {
         description: 'Pedidos para Enviar',
         url: '#'
       }, {
-        description: 'Mapa Comparativo',
+        description: 'Mapa Comparativos',
         url: '#'
       }]
     }]
